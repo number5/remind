number5/remind

commit 98c5bf511e94c50ee19abce3212af1905026dfdd

Print banner prior to first MSG-type reminder.

src/dorem.c
@@ -868,6 +868,9 @@ static int ParseScanFrom(ParsePtr s, Trigger *t, int type)
 	}
     }
 }
+
+static int DidMsgReminder = 0;
+
 /***************************************************************/
 /*                                                             */
 /*  TriggerReminder                                            */
@@ -929,7 +932,8 @@ int TriggerReminder(ParsePtr p, Trigger *t, TimeTrig *tim, int jul)
     }
 /* If it's a MSG-type reminder, and no -k option was used, issue the banner. */
     if ((t->typ == MSG_TYPE || t->typ == MSF_TYPE) 
-	&& !NumTriggered && !NextMode && !MsgCommand) {
+	&& !DidMsgReminder && !NextMode && !MsgCommand) {
+        DidMsgReminder = 1;
 	if (!DoSubstFromString(DBufValue(&Banner), &buf,
 			       JulianToday, NO_TIME) &&
 	    DBufLen(&buf)) {

commit a01e05c1db4d1c281ad582a7c3a87b4bc2544212

Fix bug introduced in commit 98c5bf511e94c50ee19abce3212af1905026dfdd: Don't print a banner when issuing a queued reminder.

src/dorem.c
@@ -869,7 +869,6 @@ static int ParseScanFrom(ParsePtr s, Trigger *t, int type)
     }
 }
 
-static int DidMsgReminder = 0;
 
 /***************************************************************/
 /*                                                             */

src/globals.h
@@ -111,6 +111,7 @@ EXTERN  INIT(   int     LastTriggerTime, 0);
 EXTERN  INIT(   int     ShouldCache, 0);
 EXTERN  char const   *CurLine;
 EXTERN  INIT(   int     NumTriggered, 0);
+EXTERN  INIT(   int     DidMsgReminder, 0);
 EXTERN  int ArgC;
 EXTERN  char const **ArgV;
 EXTERN  INIT(   int     CalLines, CAL_LINES);

src/queue.c
@@ -128,7 +128,7 @@ void HandleQueuedReminders(void)
 
     /* Suppress the BANNER from being issued */
     NumTriggered = 1;
-
+    DidMsgReminder = 1;
     /* Turn off sorting -- otherwise, TriggerReminder has no effect! */
     SortByDate = 0;
 