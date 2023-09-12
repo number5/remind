e268bbf31de795b34bd131a2f000cbb57ee39bcb
Dianne Skoll <dianne@skoll.ca>
2023-09-12 14:38:14 -0400

    Bump version to 04.02.06

--- src/main.c
+++ src/main.c
@@ -26,16 +26,10 @@
 #endif
 
 #include <ctype.h>
-#ifdef TIME_WITH_SYS_TIME
-#include <time.h>
+#if defined(HAVE_SYS_TIME_H)
 #include <sys/time.h>
-#else
-#if defined(HAVE_SYS_TIME_H) || defined (TIME_WITH_SYS_TIME)
-#include <sys/time.h>
-#else
+#endif
 #include <time.h>
-#endif
-#endif
 
 #include <sys/types.h>
 #ifdef REM_USE_WCHAR
