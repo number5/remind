a30c467c48a58f9f0c72e2fd461682ede4122de8
Dianne Skoll <dianne@skoll.ca>
2023-10-04 09:36:25 -0400

    Revert "Guess terminal background even if stdout is not a tty"
    This reverts commit 887cd83ebe9b3cc4bdbcd65ac62b95c78a1b5ec6.

--- src/init.c
+++ src/init.c
@@ -1008,6 +1008,11 @@ guess_terminal_background(int *r, int *g, int *b)
     *g = -1;
     *b = -1;
 
+    /* Don't guess if stdout not a terminal */
+    if (!isatty(STDOUT_FILENO)) {
+        return;
+    }
+
     ttyfd = open("/dev/tty", O_RDWR);
     if (ttyfd < 0) {
         return;
